e992ebbb17cb2febddb9eead6812856f10e3fe68
Trần Nam <namtv20406@st.uel.edu.vn>
2023-04-13 12:37:56 +0700

    modified:   my-app/src/app/app-routing.module.ts 	modified:   my-app/src/app/app.component.html 	modified:   my-app/src/app/app.module.ts 	modified:   my-app/src/app/footer/footer.component.css 	modified:   my-app/src/app/header/header.component.css 	modified:   my-app/src/app/header/header.component.html

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { PaymentComponent } from './payment/payment.component';
+import { count } from 'rxjs';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PaymentTransferComponent } from './payment-transfer/payment-transfer.component';
+// import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"cart",component:CartComponent},
+  {path:"payment",component:PaymentComponent},
+  {path:"payment/payment-transfer",component:PaymentTransferComponent},
+  // {path:"breadcrumb",component:BreadcrumbComponent},
+  {path:"**",component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RountingComponent=[
+  CartComponent,
+  PaymentComponent,
+  NotFoundComponent,
+  PaymentTransferComponent,
+  // BreadcrumbComponent,
+]
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RountingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SubheaderComponent } from './subheader/subheader.component';
 import { FooterComponent } from './footer/footer.component';
 import { PopupLoginComponent } from './popup-login/popup-login.component';
+import { CartComponent } from './cart/cart.component';
+import { PaymentComponent } from './payment/payment.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PaymentTransferComponent } from './payment-transfer/payment-transfer.component';
+import { ProductsViewComponent } from './products-view/products-view.component';
+// import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,14 @@ import { PopupLoginComponent } from './popup-login/popup-login.component';
     HeaderComponent,
     SubheaderComponent,
     FooterComponent,
-    PopupLoginComponent
+    PopupLoginComponent,
+    CartComponent,
+    PaymentComponent,
+    RountingComponent,
+    NotFoundComponent,
+    PaymentTransferComponent,
+    ProductsViewComponent,
+    // BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
