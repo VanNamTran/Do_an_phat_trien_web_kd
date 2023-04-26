acea1e6aaef1221e0f803b3c16483d923d4b58c2
Trần Nam <namtv20406@st.uel.edu.vn>
2023-04-26 08:11:46 +0700

    modified:   my-app/src/app/products-view/products-view.component.ts 	modified:   my-app/angular.json 	modified:   my-app/src/app/cart/cart.component.css 	modified:   my-app/src/app/cart/cart.component.html 	modified:   my-app/src/app/cart/cart.component.ts 	modified:   my-app/src/app/products-view/products-view.component.css 	modified:   my-app/src/app/products-view/products-view.component.html 	modified:   my-server-mongodb/index.js

--- my-app/src/app/products-view/products-view.component.ts
+++ my-app/src/app/products-view/products-view.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Renderer2  } from '@angular/core';
-
+import { Component, OnInit,  } from '@angular/core';
+import { ApiProductsService } from '../services/api-products.service';
 @Component({
   selector: 'app-products-view',
   templateUrl: './products-view.component.html',
@@ -7,40 +7,23 @@ import { Component, OnInit, Renderer2  } from '@angular/core';
 })
 export class ProductsViewComponent implements OnInit {
   showAllBrands = false;
-  showAllPrices =false
+  showAllPrices =false;
   hideAllBrands = false;
-  brands: string[] = [];
-  prices:string[]=[]
+  prices:string[]=[];
+  phones:any;
+  showItem=false;
+  errMessage:string='';
+  favoriteProducts: string[] = [];
+  public customerId="ctm000001"
+  listFavoriteOfCTM:string[]=[]
+  constructor(private _service:ApiProductsService){}
   ngOnInit() {
     this.prices=["> 30 triệu","20 đến 30 triệu","10 đến 20 triệu", "7 đến 10 triệu","5 dến 7 triệu","3 đến 5 triệu","dưới 3 triệu"]
-    this.brands = ['Samsung', 'Apple', 'Huawei', 'Xiaomi', 'Oppo', 'Vivo', 'Nokia'];
-    const gridLayoutButton = document.getElementById("grid-layout") as HTMLButtonElement;
-    const listLayoutButton = document.getElementById("list-layout") as HTMLButtonElement;
+    // this.brands = ['Samsung', 'Apple', 'Huawei', 'Xiaomi', 'Oppo', 'Vivo', 'Nokia'];
+    const gridLayoutButton = document.querySelector("#grid-layout") as HTMLButtonElement;
+    const listLayoutButton = document.querySelector("#list-layout") as HTMLButtonElement;
     const productsGrid = document.querySelector(".products-list") as HTMLElement;
     const products = document.querySelectorAll(".product-info") as NodeListOf<HTMLElement>;
-    // Lấy tất cả các hình ảnh "favorite-border" và "favorite"
-    const favoriteIcons: NodeListOf<HTMLImageElement> = document.querySelectorAll(".favorite-icon img");
-
-
-
-
-    // Lặp qua từng hình ảnh và thêm sự kiện click
-    favoriteIcons.forEach((icon) => {
-      icon.addEventListener("click", () => {
-        // Kiểm tra xem hình ảnh đang hiển thị là "favorite-border" hay "favorite"
-        if (icon.classList.contains("favorite-off")) {
-          // Nếu đang hiển thị "favorite-border", thay đổi thành "favorite"
-          icon.src = "./assets/icon/favorite.png";
-          icon.classList.remove("favorite-off");
-          icon.classList.add("favorite-on");
-        } else {
-          // Nếu đang hiển thị "favorite", thay đổi thành "favorite-border"
-          icon.src = "./assets/icon/favorite_border.png";
-          icon.classList.remove("favorite-on");
-          icon.classList.add("favorite-off");
-        }
-      });
-    });
 
     // sử lý sự kiện đổi layout grid-list
     gridLayoutButton.addEventListener("click", () => {
@@ -59,5 +42,55 @@ export class ProductsViewComponent implements OnInit {
         product.classList.add("products-list");
       });
     });
+    this._service.getPhones().subscribe({
+      next: (data) => { this.phones = data },
+      error: (err) => { this.errMessage = err }
+    })
+    // this._service.getListFavorites(this.customerId).subscribe({
+    //   next: (data) => { this.listFavoriteOfCTM = data },
+    //   error: (err) => { this.errMessage = err }
+    // })
+    // for (let i = 0; i < this.listFavoriteOfCTM.length; i++){
+    //   for(let j = 0; j< this.phones.length; j++){
+    //     if(this.listFavoriteOfCTM[i]==this.phones[j]){
+    //       // const productId = {}
+    //       // const favoriteOn = document.getElementById('favorite-on-' + productId);
+    //       // const favoriteOff = document.getElementById('favorite-off-' + productId);
+    //       console.log(this.phones[j])
+    //     }
+    //     else(console.log("lỗi r"))
+    //   }
+    // }
+  }
+  toggleFavorite(event: MouseEvent) {
+    const target = event.target as HTMLImageElement;
+    const productId = target.getAttribute('data-product-id');
+    let favoriteStatus = target.getAttribute('data-favorite-status') === 'false';
+    const favoriteOn = document.getElementById('favorite-on-' + productId);
+    const favoriteOff = document.getElementById('favorite-off-' + productId);
+    const customerId=this.customerId
+    let isFavorite=favoriteStatus
+    if (favoriteOn && favoriteOff) {
+      if (!isFavorite) {
+        favoriteOn.classList.add('hidden');
+        favoriteOff.classList.remove('hidden');
+        if(productId!= null){
+          this._service.putFavorite(customerId,productId,isFavorite).subscribe({
+            next:(data)=>{this.favoriteProducts = data;console.log("remove",productId),console.log(isFavorite)},
+            error:(err) =>{this.errMessage = err}
+          })
+        }
+
+      } else {
+        favoriteOn.classList.remove('hidden');
+        favoriteOff.classList.add('hidden');
+        if(productId!= null){
+          this._service.putFavorite(customerId,productId,isFavorite).subscribe({
+            next:(data)=>{this.favoriteProducts = data; console.log("add",productId),console.log(isFavorite)},
+            error:(err) =>{this.errMessage = err}
+          })
+        }
+      }
+    }
   }
 }
